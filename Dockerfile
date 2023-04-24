fcabdd4e3cda78aef4ba4b69a712ca712369fa9d
badlydrawnface <bdface@proton.me>
2023-04-24 16:38:35 -0400

    this doesn't work [yet]

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ WORKDIR /app
 
 COPY . .
 
+# idk if i need this
+CMD ["npm", "install", "redis"]
+
 EXPOSE 80
 
 CMD ["node", "app.js"]
--- app.js
+++ app.js
@@ -1,28 +1,14 @@
-const http = require('http')
 const port = 80
+import Redis from "ioredis";
+const redis = new Redis();
 
-function readCount() {
+var num = redis.get('counter');
+
+function  addOne() {
+    if (num == null) {
+        redis.set('counter', 0);
+        num = redis.get('counter');
+    }
+    return redis.set('counter', num+1);
 }
-
-// Create a server object:
-const server = http.createServer(function (req, res) {
-
-    // Write a response to the client
-    res.write('Hello World')
-
-    // End the response
-    res.end()
-})
-
-// Set up our server so it will listen on the port
-server.listen(port, function (error) {
-
-    // Checking any error occur while listening on port
-    if (error) {
-        console.log('Something went wrong', error);
-    }
-    // Else sent message of listening
-    else {
-        console.log('Server is listening on port' + port);
-    }
-})
+addOne();
