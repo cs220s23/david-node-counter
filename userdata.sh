5c4dac17b27cb4aae8d5f213371cc90cda64dc94
badlydrawnface <bdface@proton.me>
2023-04-26 17:29:35 -0400

    removed && in userdata

--- userdata.sh
+++ userdata.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo yum in git docker -y && git clone https://github.com/cs220s23/david-node-counter && cd david-node-counter || exit
+sudo yum in git docker -y 
+git clone https://github.com/cs220s23/david-node-counter
+cd david-node-counter || exit
 sudo systemctl enable --now docker
