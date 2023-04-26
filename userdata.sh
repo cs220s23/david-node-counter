84e6b1b6ffbcecbddb08f371fef22ab68f9096a9
badlydrawnface <bdface@proton.me>
2023-04-26 19:48:12 -0400

    removed sudo from user data

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo yum in git docker -y 
+yum in git docker -y
 git clone https://github.com/cs220s23/david-node-counter
 cd david-node-counter || exit
-sudo systemctl enable --now docker
+systemctl enable --now docker
